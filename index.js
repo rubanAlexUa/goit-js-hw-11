dcc8479a03852e83060ffbcddf1f6d96be6f9b33
rubanAlexUa <113380721+rubanAlexUa@users.noreply.github.com>
2025-02-19 20:30:12 +0000

    Deploying to gh-pages from @ rubanAlexUa/goit-js-hw-11@c77561691a06b6b8a3f41b85cadcbc69e50640fa 🚀

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import{a as l,i as c,S as d}from"./assets/vendor-DH2CaQaJ.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();function n(o){return l.get("https://pixabay.com/api/",{params:{key:"48844488-dc89d444e3fc14bdb2114dc7c",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(t=>(console.log(t),t.data.hits)).catch(t=>(c.error({position:"topRight",message:`Sorry, there are no images matching your search ${o}. Please try again!`}),[]))}n("/cat");function m(o,t){const i=document.querySelector(t),a=o.map(r=>`
+import{a,S as c,i as l}from"./assets/vendor-CNpXvI_o.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const n of e.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function i(t){if(t.ep)return;t.ep=!0;const e=s(t);fetch(t.href,e)}})();function d(o){return a.get("https://pixabay.com/api/",{params:{key:"48844488-dc89d444e3fc14bdb2114dc7c",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(r=>(console.log(r),r.data.hits)).catch(r=>[])}function u(o,r){const s=document.querySelector(r),i=o.map(e=>`
         <li class="gallery-item">
-            <a class='gallery-link' href="${image.largeImageURL}">
+            <a class='gallery-link' href="${e.largeImageURL}">
               <img class="li-img"
-              src="${image.webformatURL}" 
-              alt="${image.tags}/> 
+              src="${e.webformatURL}" 
+              alt="${e.tags}/> 
               <div class="li-text">
                 <table class="table">
                     <tr><td>Likes</td><td>Views</td><td>Comment</td><td>Downloads</tr>
-                    <tr><td>>${image.likes}</td><td>${image.views}</td><td>${image.comments}</td><td>${image.downloads}</tr>
+                    <tr><td>>${e.likes}</td><td>${e.views}</td><td>${e.comments}</td><td>${e.downloads}</tr>
                 </table>   
                 </div>
               </a>
             </li>
-        `).join("");i.insertAdjacentElement("afterbegin",a),new d(t+" a",{captionsData:"alt",captionDelay:250}).refresh()}const u=document.querySelector(".form-find-img");u.addEventListener("submit",o=>{o.preventDefault();const t=o.currentTarget.elements.input.value;n(t).then(i=>{i.length>0?m(i,".gallery"):console.warn("No images found")})});
+        `).join("");s.insertAdjacentHTML("afterbegin",i),new c(r+" a",{captionsData:"alt",captionDelay:250}).refresh()}const f=document.querySelector(".form-find-img");f.addEventListener("submit",o=>{o.preventDefault();const r=o.currentTarget.elements.input.value;d(r).then(s=>{s.length>0?u(s,".gallery"):l.error({position:"topRight",message:`Sorry, there are no images matching your search ${r}. Please try again!`})})});
 //# sourceMappingURL=index.js.map
