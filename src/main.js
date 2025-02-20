e970fcc5779331a69e81dedb8879c773806ded59
rubanAlexUa <rubashka2021@gmail.com>
2025-02-20 17:55:58 +0200

    message

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form-find-img');
+const message = document.querySelector('.message');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  const searchQuery = e.currentTarget.elements.input.value;
+  message.textContent = 'Wait, the image is loaded';
+  const searchQuery = e.currentTarget.elements.input.value.trim();
   getImage(searchQuery).then(images => {
     if (images.length > 0) {
       make(images, '.gallery');
@@ -18,4 +20,5 @@ form.addEventListener('submit', e => {
       });
     }
   });
+  message.textContent = '';
 });
