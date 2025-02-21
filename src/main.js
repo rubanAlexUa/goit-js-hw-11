ba6fc18940e9cd62c7ac8464d481ab902290f555
rubanAlexUa <rubashka2021@gmail.com>
2025-02-21 14:30:31 +0200

    add corrections

--- src/main.js
+++ src/main.js
@@ -5,20 +5,27 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form-find-img');
 const message = document.querySelector('.message');
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  message.textContent = 'Wait, the image is loaded';
+  message.innerHTML = 'Wait, the image is loaded';
+  console.log(message.textContent);
   const searchQuery = e.currentTarget.elements.input.value.trim();
-  getImage(searchQuery).then(images => {
-    if (images.length > 0) {
-      make(images, '.gallery');
-    } else {
-      iziToast.error({
-        position: 'topRight',
-        message: `Sorry, there are no images matching your search ${searchQuery}. Please try again!`,
-      });
-    }
-  });
-  message.textContent = '';
+  gallery.innerHTML = '';
+  getImage(searchQuery)
+    .then(images => {
+      if (images.length > 0) {
+        make(images, '.gallery');
+      } else {
+        iziToast.error({
+          position: 'topRight',
+          message: `Sorry, there are no images matching your search ${searchQuery}. Please try again!`,
+        });
+      }
+      message.textContent = '';
+    })
+    .catch(errror => {
+      gallery.innerHTML = '';
+    });
 });
